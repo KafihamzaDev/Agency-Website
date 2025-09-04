25cffff80c855c9e7736409e49a4658550504df2
KafihamzaDev <elkafihamza.dev@gmail.com>
2025-09-04 17:10:34 +0100

    header v2

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Variables
 const barsBtn = document.querySelector(".bars");
 const closeBtn = document.querySelector(".close");
-const headerNavbar = document.querySelector(".navbar");
+const navOne = document.querySelector(".nav-one");
 
 barsBtn.onclick = function () {
-  headerNavbar.style.display = "block";
+  navOne.style.display = "flex";
 };
 
 closeBtn.onclick = function () {
-  headerNavbar.style.display = "none";
+  navOne.style.display = "none";
 };

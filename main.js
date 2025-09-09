0f4bbdfc5f46140496fe60e219f07d0ac8a8b668
KafihamzaDev <elkafihamza.dev@gmail.com>
2025-09-09 13:00:57 +0100

    final header

--- main.js
+++ main.js
@@ -12,3 +12,42 @@ closeBtn.onclick = function () {
 };
 
 // change header content when window scroll down
+// Variables
+const header = document.querySelector(".header");
+const logoDark = document.querySelector(".logo-dark");
+const logoLight = document.querySelector(".logo-light");
+const ulNavOne = document.querySelectorAll(".navbar-list-one .navbar-link");
+const ulNavTwo = document.querySelectorAll(".navbar-list-two .navbar-link");
+const headerBtn = document.querySelector(".header-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
+    header.style.backgroundColor = "var(--white)";
+    logoLight.style.display = "none";
+    logoDark.style.display = "block";
+    ulNavOne.forEach(function (el) {
+      el.style.color = "var(--white)";
+    });
+    ulNavTwo.forEach(function (el) {
+      el.style.color = "var(--raisin-black)";
+    });
+    headerBtn.style.backgroundColor = "var(--violet-blue-crayola)";
+    headerBtn.style.color = "var(--white)";
+    barsBtn.style.color = "var(--charcoal)";
+  } else if (window.scrollY < 100) {
+    header.style.backgroundColor = "";
+    logoLight.style.display = "";
+    logoDark.style.display = "";
+    ulNavOne.forEach(function (el) {
+      el.style.color = "";
+    });
+    ulNavTwo.forEach(function (el) {
+      el.style.color = "";
+    });
+    headerBtn.style.backgroundColor = "";
+    headerBtn.style.color = "";
+    barsBtn.style.color = "";
+  }
+});
+
+console.log(headerBtn);
